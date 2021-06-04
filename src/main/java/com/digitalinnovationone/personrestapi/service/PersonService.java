8c292381a78e51d9da5f94e06f465148db0f1a85
Maju <majutrevisan@gmail.com>
2021-06-04 13:30:26 -0300

    Create 'list all' method

--- src/main/java/com/digitalinnovationone/personrestapi/service/PersonService.java
+++ src/main/java/com/digitalinnovationone/personrestapi/service/PersonService.java
@@ -3,15 +3,21 @@ package com.digitalinnovationone.personrestapi.service;
 import com.digitalinnovationone.personrestapi.dto.request.PersonDTO;
 import com.digitalinnovationone.personrestapi.dto.response.MessageResponseDTO;
 import com.digitalinnovationone.personrestapi.entity.Person;
+import com.digitalinnovationone.personrestapi.mapper.PersonMapper;
 import com.digitalinnovationone.personrestapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PersonService {
 
     private PersonRepository personRepository;
+
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
     @Autowired
     public PersonService(PersonRepository personRepository){
@@ -19,12 +25,19 @@ public class PersonService {
     }
 
     public MessageResponseDTO createPerson(@RequestBody PersonDTO personDTO){
-        Person pe
+        Person personToCreate = personMapper.toModel(personDTO);
 
-        Person newPerson = personRepository.save(personDTO);
+        Person newPerson = personRepository.save(personToCreate);
         return MessageResponseDTO
                 .builder()
                 .message("Created new person with ID " + newPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> all = personRepository.findAll();
+        return all.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
