49c804b0c473d6a6e98ddc1f859edcc58f825a74
Maju <majutrevisan@gmail.com>
2021-06-06 16:12:55 -0300

    Add 'delete by Id' method

--- src/main/java/com/digitalinnovationone/personrestapi/service/PersonService.java
+++ src/main/java/com/digitalinnovationone/personrestapi/service/PersonService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -44,10 +43,20 @@ public class PersonService {
     }
 
     public PersonDTO findById(Long id) throws PersonNotFoundException {
-        Person person = personRepository.findById(id)
-                .orElseThrow(() -> new PersonNotFoundException(id));
+        Person person = checkIfPersonExists(id);
 //        Optional<Person> optPerson = personRepository.findById(id);
 
         return personMapper.toDTO(person);
+    }
+
+    public void delete(Long id) throws PersonNotFoundException {
+        checkIfPersonExists(id);
+
+        personRepository.deleteById(id);
+    }
+
+    private Person checkIfPersonExists(Long id) throws PersonNotFoundException {
+        return personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException(id));
     }
 }
